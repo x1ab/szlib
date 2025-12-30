54d319337fbe35962870d7c6e27fbfa7e4e117da
sz <698307+xparq@users.noreply.github.com>
2025-12-30 14:53:58 +0100

    IGNORE: Quote the redef. warning

--- src/lang/IGNORE.hh
+++ src/lang/IGNORE.hh
@@ -1,22 +1,24 @@
-// The "perfect unused-ignorer" hack:
+// The (not) perfect unused-warning suppressor hack...
 // Usage: f(A unused1, B unused2) { IGNORE unused1, unused2; }
 // Sexy points over other solutions:
-// - No need to mess with the function's signature
-// - No parentheses (they're just annoying to type)
+// - No parentheses (they're so annoying to type)
+// - No need to mess with the function signature
 // - No ridiculous [[maybe_unused]] for every var
-// - No relying on multi-GB IDEs to do the above for ya
-// Collisions? Be more careful whom you befriend next time...
-// This header can be re-#included freely to reapply the macro,
-// should a villain try to re-#define IGNORE away from you.
+// - No relying on a GB-sized IDE to do all these
+// Macro collisions? Well, be more careful whom you befriend next time!... ;)
+// But should a villain #define IGNORE away from you, this header can just
+// be #included again freely to override, stealing the IGNORE macro back.
+// (Windows.h is the archetypal enemy, and it almost never really needs
+// that macro anyway.)
 
 #ifdef IGNORE
 // We're kinda losing against winbase.h here...
 # ifndef _MSC_VER
-#  warning Oh no, someone beat us to it: IGNORE is already taken! ;) But never fear, let's just #undef it like a boss...
-# else
+#  warning                  "Oh no, someone beat us to it: IGNORE is already taken! :-/ But never fear, let's just #undef it and win the boss fight..."
+# else                      // Without the quotes, GCC was freaking out about the apostrophe here..........^
 #  define _sz__STR_2(x) #x
 #  define _sz__STR(x) _sz__STR_2(x)
-#  pragma message( "Warning: Oh no, someone beat us to it: IGNORE is already taken! ;) But never fear, let's just #undef it like a boss..." \
+#  pragma message( "Warning: Oh no, someone beat us to it: IGNORE is already taken! :-/ But never fear, let's just #undef it and win the boss fight..." \
 	" at " __FILE__ ":" _sz__STR(__LINE__)  )
 #  undef _sz__STR
 #  undef _sz__STR_2
@@ -26,8 +28,9 @@
 #
 #endif // IGNORE
 
-#define IGNORE  _sz__object_sinkhole_{},
+#define IGNORE  _sz__object_sinkhole_{}, // You'd think "hmm, tack a [[maybe_unused]] in front, to avoid the irony..."
+                                         // — but no: C++ is not your friend. It's your enemy. It wouldn't compile. :-/
 #ifndef         _sz__object_sinkhole__defined_ // Only guard this bit, not the whole header!
 #define         _sz__object_sinkhole__defined_
-  struct        _sz__object_sinkhole_{ template<typename T> _sz__object_sinkhole_& operator,(const T&) { return *this; } };
+  struct _sz__object_sinkhole_{ template<typename T> constexpr _sz__object_sinkhole_& operator,(const T&) { return *this; } };
 #endif
